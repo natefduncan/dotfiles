937ebf50079cd0b8779753231616da88b5ebf0a6
Nate Duncan <natefduncan@gmail.com>
2022-02-23 16:36:53 -0600

    Github CLI

--- install.sh
+++ install.sh
@@ -58,3 +58,12 @@ rm -rf node-$NODE_VERSION-$DISTRO.tar.xz
 # Add to and .bashrc
 echo "export PATH=\"/usr/local/lib/nodejs/node-$NODE_VERSION-$DISTRO/bin:\$PATH:\"" >> ~/.bashrc
 . ~/.bashrc
+
+# GitHub CLI
+if [ $OS == mac ]; then
+	brew install gh
+elif [ $OS == linux ]
+	curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+	sudo apt install gh
+fi
