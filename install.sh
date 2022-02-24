e8607bf6ad19eb723e1dd1047b76e3f422947bd7
natefduncan <natefduncan@gmail.com>
2022-02-23 23:30:03 -0600

    Add slack config via read

--- install.sh
+++ install.sh
@@ -140,4 +140,12 @@ elif [ "$OS" == "linux" ]; then
 	DISTRO=linux-amd64
 fi
 curl -OL "https://github.com/erroneousboat/slack-term/releases/download/v$SLACK_VERSION/slack-term-$DISTRO"
+
 mv slack-term-$DISTRO /usr/local/bin/slack-term
+# Add slack token to config file 
+# { "slack_token" : "TOKEN" }
+# at ~/.config/slack-term/configss
+mkdir -p ~/.config/slack-term
+echo "Input slack token"
+read slack_token
+echo "{ \"slack_token\":\"${slack_token}\"}" >> ~/.slack-term
