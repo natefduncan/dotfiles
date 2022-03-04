49de14fa1a7d38e5d110b0c7ca7f48044e8da86d
Nate Duncan <natefduncan@gmail.com>
2022-03-04 16:34:56 -0600

    Add tmp folder

--- src/copy_conf.sh
+++ src/copy_conf.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Create ~/.vim
-mkdir -p ~/.vim/{autoload,plugged}
+mkdir -p ~/.vim
+mkdir -p ~/.vim/tmp
 
 # Copy dotfiles to home
-cp -r .vim/ ~/.vim
 cp .vimrc ~/.vimrc
 cp .tmux.conf ~/.tmux.conf
 
