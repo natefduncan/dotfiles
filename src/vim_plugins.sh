fdc140bacc7eac58f83301924f7aa18be0d21466
natefduncan <natefduncan@gmail.com>
2022-03-05 12:33:02 -0600

    Fix: Change to one command

--- src/vim_plugins.sh
+++ src/vim_plugins.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
 # COC Analyzers
-# Rust
-vim -c :CocInstall coc-rust-analyzer
-
-# Python
-vim -c :CocInstall coc-pyright
-
-# R
-# Must install.packages("languageserver")
-vim -c :CocInstall coc-r-lsp
-
-# JSON
-vim -c :CocInstall coc-json
-
-# Markdown
-vim -c :CocInstall coc-markdownlint
-
-# Javascript
-vim -c :CocInstall coc-tsserver
-
+# Rust, HTML, CSS, Python, R, JSON, Markdown, Javascript
+vim -c 'CocInstall -sync coc-rust-analyzer coc-html coc-css coc-pyright coc-r-lsp coc-json coc-markdown coc-tsserver|q'
