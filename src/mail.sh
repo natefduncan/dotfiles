18a2856df7274d9e71834c71a4492a372a6e47fd
Nate Duncan <natefduncan@gmail.com>
2022-03-08 10:14:16 -0600

    Add: Urlview

--- src/mail.sh
+++ src/mail.sh
@@ -21,13 +21,14 @@ if [ "$OS" == "linux" ]; then
 	# Required
 	sudo apt-get install -y neomutt isync msmtp 
 	# Optional
-	sudo apt-get install -y lynx notmuch abook
+	sudo apt-get install -y lynx notmuch abook urlview 
 elif [ "$OS" == "mac" ]; then
 	# Required
 	brew install neomutt isync msmtp pass
 	# Options
 	brew install lynx notmuch abook
 fi
+# Optional pam-gnupg
 # gpg --full-gen-key
 # pass init EMAIL_ADDRESS 
 # Enable IMAP forwarding 
