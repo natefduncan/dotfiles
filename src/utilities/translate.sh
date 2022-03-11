b8bb3adc9c290779c09d83600c3176620e15c559
Nate Duncan <natefduncan@gmail.com>
2022-03-11 10:06:09 -0600

    Refactor: Move translate-shell to /usr/local/src

--- src/utilities/translate.sh
+++ src/utilities/translate.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 git clone https://github.com/soimort/translate-shell.git
-cd translate-shell
+sudo mv translate-shell/ /usr/local/src/
+cd /usr/local/src/translate-shell
 make
 sudo make install
-cd .. 
-rm -rf translate-shell
