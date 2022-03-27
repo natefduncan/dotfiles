869bd4e135d1d6ef0c086bce2d7393e79738bb0a
natefduncan <natefduncan@gmail.com>
2022-03-27 13:08:40 -0500

    Add: Clockify-rs

--- src/clockify.sh
+++ src/clockify.sh
@@ -1,28 +1,3 @@
 #!/bin/bash
 
-# Set Variables
-if [ "$(uname)" == "Darwin" ]; then
-	OS="mac"
-	DISTRO="darwin-x64"
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-	OS="linux"
-	DISTRO="linux-x64"
-fi
-
-# CLOCKIFY
-CLOCKIFY_VERSION=0.32.0
-if [ "$OS" == "mac" ]; then
-	brew install lucassabreu/tap/clockify-cli
-elif [ "$OS" == "linux" ]; then
-	# Download and unzip binary
-	wget "https://github.com/lucassabreu/clockify-cli/releases/download/v$CLOCKIFY_VERSION/clockify-cli_${CLOCKIFY_VERSION}_Linux_x86_64.tar.gz"
-	sudo mkdir -p /usr/local/lib/clockify
-	tar -xf clockify-cli_$(CLOCKIFY_VERSION)_Linux_x86_64.tar.gz -C /usr/local/clockify
-	rm -rf clockify-cli_$(CLOCKIFY_VERSION)_Linux_x86_64.tar.gz
-
-	# Add to .bashrc
-	echo "export PATH=\"/usr/local/lib/clockify:\$PATH\"" >> ~/.bashrc
-	. ~/.bashrc
-fi
-clockify-cli config --init
-
+cargo install clockify
